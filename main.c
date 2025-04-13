c3cf01b46d8c7452baefce14b32574eae5e0a340
Leonisaurov <leonisaurov@gmail.com>
2025-04-12 18:14:25 -0600

    Added an option to select the command to use

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include "terminal.h"
 #include "command.h"
 
+#include <stdlib.h>
 #include <unistd.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     int flags;
 
     struct termios term = set_raw_terminal(STDIN_FILENO);
     set_nonblockig_input(STDIN_FILENO, &flags);
     unset_buffering(stdout);
 
-    encapsule_cmd(term, "bash");
+    char* cmd;
+    if (argc == 2)
+        cmd = argv[1];
+    else
+        cmd = getenv("SHELL");
+    encapsule_cmd(term, cmd);
 
     set_buffering(stdout);
     set_normal_input(STDIN_FILENO, flags);
